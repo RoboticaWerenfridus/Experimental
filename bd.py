4996247047051d7e9648cd495fa0d3b8c0dc6990
N.s <100487618+NathanSchalkwijk@users.noreply.github.com>
2025-01-31 17:27:22 +0100

    Create bd.py

---
+++ bd.py
@@ -0,0 +1,26 @@
+from gpiozero import Robot
+import sys
+import tkinter as tk
+from bluedot import BlueDot
+
+robot = Robot(left=(18, 17), right=(23, 22))
+
+bd = BlueDot()
+
+def move(pos):
+    if pos.top:
+        robot.forward()
+    elif pos.left:
+        robot.left()
+    elif pos.right:
+        robot.right()
+    elif pos.bottom:
+        robot.backward()
+
+def stop():
+    robot.stop()
+
+bd.when_pressed = move
+bd.when_released = stop
+command = tk.Tk()
+command.mainloop()
